427eaa02c9c263adc2df35abe186a705a2d4ec3c
v-zhumin <v-zhumin@V-ZHUMIN-W10>
2017-01-11 10:28:08 +0800

    Add project files.

---
+++ App39/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App39
+{
+   public class ViewModel :INotifyPropertyChanged
+    {
+        private decimal template;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public decimal Template
+        {
+            get
+            {
+                return template;
+            }
+
+            set
+            {
+                template = value;
+                OnPropertyChanged();
+            }
+        }
+
+       public ViewModel()
+        {
+            template = new decimal(15);
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
